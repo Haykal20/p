b3f7058291ca7e1a21cd8866f8255256fa7b8880
Haykal20 <124482827+Haykal20@users.noreply.github.com>
2025-03-18 10:34:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname)));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: true
 }));
 
 // Rute untuk menangani permintaan GET ke root URL
